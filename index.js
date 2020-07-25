34243c9b885245a8a3cea2782aa296977c0fa142
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-07-25 23:43:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 function dwarfRollCall(dwarves) {
-}
+  var array2 = []
+  for (let i = 0; i < dwarves.length; i++) {
+   array2.push(`${[i + 1]}. ${dwarves[i]} `);
+  } return array2.join("")
+} 
 
-function summonCaptainPlanet(planeteerCalls){
-}
-
+function summonCaptainPlanet(planeteerCalls) {
+  var array = [];
+  for (let i = 0; i <planeteerCalls.length; i++) {
+    array.push(`${planeteerCalls[i]}!`.toUpperCase());
+  } return array;
+}  
 function longPlaneteerCalls(words) {
+    if (words.find(i => i.length > 4)) {
+      return true;
+    } else {
+      return false
+    }
 }
 
 function findTheCheese (foods) {
+  if (foods.find(i => i == "cheddar", "gouda", "camembert")) {
+    return foods.find(i => i == "cheddar", "gouda", "camembert");
+  } else {
+    return 'no cheese!'
+  }
 }
