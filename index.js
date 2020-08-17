aff6309b9245a4a877e2c2dc9b21f493b8913ebd
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-08-17 15:29:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function dwarfRollCall(dwarves) {
-  var array2 = []
-  for (let i = 0; i < dwarves.length; i++) {
+  var array2 = [];
+  for (let i = dwarves.length / 2; i < dwarves.length; i++) {
    array2.push(`${[i + 1]}. ${dwarves[i]} `);
-  } return array2.join("")
+  } return array2.join("");
 } 
 
 function summonCaptainPlanet(planeteerCalls) {
@@ -12,17 +12,28 @@ function summonCaptainPlanet(planeteerCalls) {
   } return array;
 }  
 function longPlaneteerCalls(words) {
-    if (words.find(i => i.length > 4)) {
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
       return true;
     } else {
-      return false
+      return false;
     }
+  }
 }
 
 function findTheCheese (foods) {
-  if (foods.find(i => i == "cheddar", "gouda", "camembert")) {
-    return foods.find(i => i == "cheddar", "gouda", "camembert");
+  if (foods.find(i => i == "cheddar", "gouda", "camembert", "swiss")) {
+    return foods.find(i => i == "cheddar", "gouda", "camembert", "swiss");
   } else {
-    return 'no cheese!'
+    return 'no cheese!';
   }
+}
+
+function wordsWithB (words) {
+  var array = []
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].charAt(0).toLowerCase() == "b") {
+    array.push(words[i])
+    }
+  } return array;
 }
